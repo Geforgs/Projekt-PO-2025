4ec9a1835116a88aec37648b33610b28180a300f
D3cey <gazej007@gmail.com>
2025-04-20 20:41:05 +0200

    Changes...

---
+++ platform-api/src/main/java/po25/Contest.java
@@ -0,0 +1,4 @@
+package po25;
+
+public interface Contest {
+}
--- platform-api/src/main/java/po25/Main.java
+++ platform-api/src/main/java/po25/Main.java
@@ -1,7 +1,4 @@
 package po25;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
 }
