d985ad3671e664a7a1cec30cde4c0c11a6945b3e
D3cey <gazej007@gmail.com>
2025-05-11 19:34:16 +0200

    Move PlatformException to another file

---
+++ platform-api/src/main/java/po25/PlatformException.java
@@ -0,0 +1,11 @@
+package po25;
+
+public class PlatformException extends Exception {
+    public PlatformException(String message) {
+        super(message);
+    }
+
+    public PlatformException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
