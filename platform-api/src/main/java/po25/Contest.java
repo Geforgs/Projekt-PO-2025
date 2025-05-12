ac65059c33e5f1d3a7b7ad409adfdbb066e00b63
D3cey <gazej007@gmail.com>
2025-05-12 02:52:39 +0200

    Add getId method to Contest interface, type should be changed to String from long in the future

--- platform-api/src/main/java/po25/Contest.java
+++ platform-api/src/main/java/po25/Contest.java
@@ -8,27 +8,38 @@ import java.time.LocalDateTime;
  * Interface representing a single contest (competition).
  */
 public interface Contest {
+    /**
+     * Returns the unique identifier of the contest on its platform.
+     * This ID is used to retrieve details for this specific contest.
+     *
+     * @return The unique identifier of the contest (e.g., "1742", "Div2RoundXYZ").
+     */
+    long getId();
 
     /**
      * Returns the title of the contest.
+     *
      * @return title of the contest.
      */
     String getTitle();
 
     /**
      * Returns the list of tasks available within this contest.
+     *
      * @return list of Task objects.
      */
     List<Task> getTasks() throws PlatformException;
 
     /**
      * Returns the description of the contest, if available.
+     *
      * @return Optional containing the description or an empty Optional.
      */
     Optional<String> getDescription();
 
     /**
      * Searches for and returns a task based on its identifier.
+     *
      * @param taskId task identifier.
      * @return Optional containing the Task object if a task with the given ID exists, otherwise an empty Optional.
      */
@@ -36,12 +47,14 @@ public interface Contest {
 
     /**
      * Returns the start date and time of the contest, if specified.
+     *
      * @return Optional containing the start date and time or an empty Optional.
      */
     Optional<LocalDateTime> getStartTime();
 
     /**
      * Returns the end date and time of the contest, if specified.
+     *
      * @return Optional containing the end date and time or an empty Optional.
      */
     Optional<LocalDateTime> getEndTime();
