37ef5df23b9f221cb387b4e0e106a591c5335f0f
D3cey <gazej007@gmail.com>
2025-05-24 21:44:14 +0200

    Modify core package to enable support for SatoriPlatform

--- core/src/main/java/po25/service/PlatformService.java
+++ core/src/main/java/po25/service/PlatformService.java
@@ -1,11 +1,7 @@
 package po25.service;
 
-import po25.Contest;
-import po25.Platform;
-import po25.PlatformException;
-import po25.Task;
+import po25.*;
 
-import po25.CodeforcesPlatform;
 // import po25.SatoriPlatform;
 
 import java.util.Collections;
@@ -31,12 +27,12 @@ public class PlatformService {
             System.err.println("Error initializing Codeforces platform: " + e.getMessage());
         }
 
-        // try {
-        //     Platform satori = new SatoriPlatform();
-        //     registeredPlatforms.put(satori.getPlatformName().toLowerCase(), satori);
-        // } catch (Exception e) {
-        //     System.err.println("Error initializing Satori platform: " + e.getMessage());
-        // }
+         try {
+             Platform satori = new SatoriPlatform();
+             registeredPlatforms.put(satori.getPlatformName().toLowerCase(), satori);
+         } catch (Exception e) {
+             System.err.println("Error initializing Satori platform: " + e.getMessage());
+         }
 
     }
 
