a1360466e3a6aa2775562321e51a819d315d452c
Pavlo Tsikalyshyn <pavlo.tsikalyshyn@student.uj.edu.pl>
2025-05-30 12:43:56 +0200

    Added browser for everyone.

--- browser/src/main/java/po25/Browser.java
+++ browser/src/main/java/po25/Browser.java
@@ -3,22 +3,42 @@ package po25;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Browser {
-    private static boolean started = true;
+    private static boolean started = false;
+    private static Process process;
+    private static String pathToChrome;
 
     static public ChromeDriver getChrome(){
         if(!started){
             throw new RuntimeException("Browser is not started");
         }
-        String pahtToChrome = "";
-        if(System.getProperty("os.name").startsWith("Mac")){
-            pahtToChrome = System.getProperty("user.dir") + "/browser/src/main/java/po25/MacOsChrome/Google Chrome.app/Contents/MacOS/Google Chrome";
-        }else if(System.getProperty("os.name").startsWith("Windows")){
-            System.out.println("Soon will be added to the chrome driver");
-            throw new RuntimeException("Windows is not added to the chrome driver");
-        }
-        ChromeOptions options = new ChromeOptions().setBinary(pahtToChrome);
+        ChromeOptions options = new ChromeOptions().setBinary(pathToChrome);
         options.setExperimentalOption("debuggerAddress", "127.0.0.1:9222");
         return new ChromeDriver(options);
+    }
+
+    static public void setPathToChrome(String pathToChrome){
+        Browser.pathToChrome = pathToChrome;
+    }
+
+    static public void start() throws IOException {
+        List<String> commands = new ArrayList<>();
+        commands.add(pathToChrome);
+        commands.add("--remote-debugging-port=9222");
+        commands.add("--user-data-dir=" + System.getProperty("user.dir") + "/browser/src/main/java/po25/cash");
+        ProcessBuilder build = new ProcessBuilder(commands);
+        process = build.start();
+        started = true;
+    }
+
+    static public void stop(){
+        process.destroy();
+        started = false;
     }
 }
